3939c3ec684e02fe5cd605ae69bd28a850bc1f72
Nur Mohammad <nm.navid.888@gmail.com>
2025-05-14 02:19:19 +0600

    Python starter problem 1 is added.

---
+++ starter_problems/starter_problem_1.py
@@ -0,0 +1,5 @@
+#Problem-1: Write a program to find the length of the variable name
+#Variable, name="Hello there"
+name="Hello there"
+print(len(name))
+print("Lenght of the variable 'name' is ", len(name))
